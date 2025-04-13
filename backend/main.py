e159e08154e1c83f5856a59f0f4dd8df55cc619e
JAM <126861194+patacjamaica1@users.noreply.github.com>
2025-04-13 21:19:56 +0800

    corrected url

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:3002",
+        "https://fastapi-frontend.netlify.app"
         ],
     allow_credentials=True,
     allow_methods=["*"],
